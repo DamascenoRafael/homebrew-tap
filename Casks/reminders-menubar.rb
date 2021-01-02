f0f2ae8fdfc5e2ba72af904b7e48326a3a0ef2a4
Rafael Damasceno <damscn@gmail.com>
2021-01-02 17:31:38 -0300

    Add appcast for reminders-menubar cask

--- Casks/reminders-menubar.rb
+++ Casks/reminders-menubar.rb
@@ -3,7 +3,7 @@ cask "reminders-menubar" do
     sha256 "fc173b709985ea0a724df788b480d15db99e344bf87c0571ddf602cf3a47ad31"
   
     url "https://github.com/DamascenoRafael/reminders-menubar/releases/download/v#{version}/reminders-menubar.zip"
-
+    appcast "https://github.com/DamascenoRafael/reminders-menubar/releases.atom"
     name "Reminders Menu Bar"
     desc "Simple macOS menu bar app to view and interact with reminders"
     homepage "https://github.com/DamascenoRafael/reminders-menubar"
