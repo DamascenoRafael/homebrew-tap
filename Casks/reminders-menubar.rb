f43401c388886466eb67a5b917c5e8594af411ba
Rafael Damasceno <damscn@gmail.com>
2021-02-09 01:32:11 -0300

    Fix sha256 of reminders-menubar 1.2.0

--- Casks/reminders-menubar.rb
+++ Casks/reminders-menubar.rb
@@ -1,6 +1,6 @@
 cask "reminders-menubar" do
   version "1.2.0"
-  sha256 "95cb106a10a88c6f1898d8ed85ed00213baadc83a276f84acf369408ad799230"
+  sha256 "1ad96190674fadb1642bd0e78ce83500f2fc0842e242a58622bc5f1afe5a6b69"
 
   url "https://github.com/DamascenoRafael/reminders-menubar/releases/download/v#{version}/reminders-menubar.zip"
   appcast "https://github.com/DamascenoRafael/reminders-menubar/releases.atom"
