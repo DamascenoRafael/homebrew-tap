deeb8e8fd3db74dc54d0d043f878c594b49e6555
Rafael Damasceno <damscn@gmail.com>
2022-05-15 22:16:36 -0300

    Update reminders-menubar from 1.6.1 to 1.7.0

--- Casks/reminders-menubar.rb
+++ Casks/reminders-menubar.rb
@@ -1,6 +1,6 @@
 cask "reminders-menubar" do
-  version "1.6.1"
-  sha256 "ecaf274774cbb030deb611d0b58039884680722d756122bf58603907443f4634"
+  version "1.7.0"
+  sha256 "eecf1608bb58aa2a5a9472b14211390a80800a987a070bdfe32b155044d8c040"
 
   url "https://github.com/DamascenoRafael/reminders-menubar/releases/download/v#{version}/reminders-menubar.zip"
   appcast "https://github.com/DamascenoRafael/reminders-menubar/releases.atom"
